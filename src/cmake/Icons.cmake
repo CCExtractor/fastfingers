4a85232c532ef3c62150bfc1f0054d8cad5ed7fa
Ali Eren Yoğurtçu <aeren.yogurtcu@gmail.com>
2021-06-27 12:49:20 +0300

    File IO controls added for applications icons that are installed to usr/share

--- src/cmake/Icons.cmake
+++ src/cmake/Icons.cmake
@@ -3,14 +3,16 @@ macro(add_icons ICONS_TARGET)
   # Installing hicolor icons
   list(APPEND HICOLOR_PX 16 32 48 64 128)
   foreach (PX IN LISTS HICOLOR_PX)
-    add_custom_command (
-      TARGET ${ICONS_TARGET}
-      POST_BUILD
-      COMMAND
-      ${CMAKE_COMMAND} -E copy
-      "${CMAKE_SOURCE_DIR}/../data/icons/hicolor/${PX}x${PX}/fastfingers.png"
-      "/usr/share/icons/hicolor/${PX}x${PX}/apps/"
-      )
+    if (EXISTS "/usr/share/icons/hicolor/${PX}x${PX}/")
+      add_custom_command (
+	TARGET ${ICONS_TARGET}
+	POST_BUILD
+	COMMAND
+	${CMAKE_COMMAND} -E copy
+	"${CMAKE_SOURCE_DIR}/../data/icons/hicolor/${PX}x${PX}/fastfingers.png"
+	"/usr/share/icons/hicolor/${PX}x${PX}/apps/"
+	)
+    endif()
   endforeach()
   # Updating hicolor cache
   add_custom_command(
@@ -21,14 +23,16 @@ macro(add_icons ICONS_TARGET)
   # Installing highcontrast icons
   list(APPEND HICONTRAST_PX 16 22 24 32 48 256)
   foreach (PX IN LISTS HICONTRAST_PX)
-    add_custom_command (
-      TARGET fastfingers
-      POST_BUILD
-      COMMAND
-      ${CMAKE_COMMAND} -E copy
-      "${CMAKE_SOURCE_DIR}/../data/icons/HighContrast/${PX}x${PX}/fastfingers.png"
-      "/usr/share/icons/HighContrast/${PX}x${PX}/apps/"
-      )
+    if (EXISTS "/usr/share/icons/HighContrast/${PX}x${PX}/")
+      add_custom_command (
+	TARGET fastfingers
+	POST_BUILD
+	COMMAND
+	${CMAKE_COMMAND} -E copy
+	"${CMAKE_SOURCE_DIR}/../data/icons/HighContrast/${PX}x${PX}/fastfingers.png"
+	"/usr/share/icons/HighContrast/${PX}x${PX}/apps/"
+	)
+    endif()
   endforeach()
   # Updating highcontrast cache
   add_custom_command(
