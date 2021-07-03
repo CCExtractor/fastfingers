5caa86a50640e6e324e581c94a1d82ff838919bc
Ali Eren Yoğurtçu <aeren.yogurtcu@gmail.com>
2021-07-03 23:33:28 +0300

    Settings page list style improved

--- src/cheatsheet.c
+++ src/cheatsheet.c
@@ -331,7 +331,7 @@ main (int    argc,
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("org.ccextractor.Fastfingers.cheatsheet", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new ("org.ccextractor.FastFingers", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
