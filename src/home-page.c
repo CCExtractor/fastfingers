f08199696053e90871dc3d2d6fa5a06c0f7d6acc
Ali Eren Yogurtcu <aeren.yogurtcu@gmail.com>
2021-07-11 12:27:56 +0300

    Removed filler objects, gtk align used instead

--- src/home-page.c
+++ src/home-page.c
@@ -15,30 +15,11 @@ void ff_home_page_init(GtkStack *stack) {
   GtkWidget *hcont = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
   gtk_style_context_add_class(gtk_widget_get_style_context(hcont),
                               "home-hcont");
+  gtk_widget_set_halign(hcont, GTK_ALIGN_CENTER);
   gtk_box_pack_start(GTK_BOX(container), hcont, FALSE, FALSE, 0);
-
-  GtkWidget *lfiller = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-  gtk_widget_set_hexpand(lfiller, FALSE);
-  gtk_box_pack_start(GTK_BOX(hcont), lfiller, FALSE, FALSE, 0);
-
-  GtkWidget *gmail = ff_card_new("Gmail", 0.4);
-  gtk_box_pack_start(GTK_BOX(hcont), gmail, FALSE, FALSE, 0);
-
-  GtkWidget *slack = ff_card_new("Slack", 0.23);
-  gtk_box_pack_start(GTK_BOX(hcont), slack, FALSE, FALSE, 0);
-
-  GtkWidget *vscode = ff_card_new("VS Code", 0.16);
-  gtk_box_pack_start(GTK_BOX(hcont), vscode, FALSE, FALSE, 0);
-
-  GtkWidget *sketch = ff_card_new("Sketch", 0.04);
-  gtk_box_pack_start(GTK_BOX(hcont), sketch, FALSE, FALSE, 0);
 
   GtkWidget *firefox = ff_card_new("Firefox", 0.04);
   gtk_box_pack_start(GTK_BOX(hcont), firefox, FALSE, FALSE, 0);
-
-  GtkWidget *rfiller = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-  gtk_widget_set_hexpand(rfiller, FALSE);
-  gtk_box_pack_start(GTK_BOX(hcont), rfiller, FALSE, FALSE, 0);
 
   gtk_stack_add_named(GTK_STACK(stack), GTK_WIDGET(scrolled_window), "home");
   gtk_widget_show_all(GTK_WIDGET(scrolled_window));
