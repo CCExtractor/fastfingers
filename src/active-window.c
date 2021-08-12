02a47dae8c0676ce340fc79d916865ae84d25945
Ali Eren Yogurtcu <aeren.yogurtcu@gmail.com>
2021-08-12 18:21:02 +0300

    home page recent fixed

--- src/active-window.c
+++ src/active-window.c
@@ -5,119 +5,119 @@
 #include <X11/Xlib.h>
 
 int check_status(int status, unsigned long window) {
-  if (status == BadWindow) {
-    fprintf(stderr, "FF-ERROR: window id # 0x%lx does not exists!\n", window);
-    return 1;
-  }
+    if (status == BadWindow) {
+        fprintf(stderr, "FF-ERROR: window id # 0x%lx does not exists!\n", window);
+        return 1;
+    }
 
-  if (status != Success) {
-    fprintf(stderr, "FF-ERROR :XGetWindowProperty failed!\n");
-    return 2;
-  }
+    if (status != Success) {
+        fprintf(stderr, "FF-ERROR :XGetWindowProperty failed!\n");
+        return 2;
+    }
 
-  return 0;
+    return 0;
 }
 
 char *get_active_from_xorg(void) {
-  Display *display;
-  Atom actual_type, filter_atom;
-  int screen_number, actual_format, status, check;
-  unsigned long root_window, active_window, nitems, bytes_after;
-  unsigned char *prop;
+    Display *display;
+    Atom actual_type, filter_atom;
+    int screen_number, actual_format, status, check;
+    unsigned long root_window, active_window, nitems, bytes_after;
+    unsigned char *prop;
 
-  display = XOpenDisplay(NULL);
-  if (!display) {
-    fprintf(stderr, "FF-ERROR: Unable to open display '%s'\n",
-            XDisplayName(NULL));
-    return NULL;
-  }
+    display = XOpenDisplay(NULL);
+    if (!display) {
+        fprintf(stderr, "FF-ERROR: Unable to open display '%s'\n",
+                XDisplayName(NULL));
+        return NULL;
+    }
 
-  screen_number = XDefaultScreen(display);
-  root_window = XRootWindow(display, screen_number);
-  filter_atom = XInternAtom(display, "_NET_ACTIVE_WINDOW", True);
-  status = XGetWindowProperty(display, root_window, filter_atom, 0, 1024, False,
-                              AnyPropertyType, &actual_type, &actual_format,
-                              &nitems, &bytes_after, &prop);
-  check = check_status(status, root_window);
-  if (check) {
+    screen_number = XDefaultScreen(display);
+    root_window = XRootWindow(display, screen_number);
+    filter_atom = XInternAtom(display, "_NET_ACTIVE_WINDOW", True);
+    status = XGetWindowProperty(display, root_window, filter_atom, 0, 1024, False,
+                                AnyPropertyType, &actual_type, &actual_format,
+                                &nitems, &bytes_after, &prop);
+    check = check_status(status, root_window);
+    if (check) {
+        XCloseDisplay(display);
+        return NULL;
+    }
+
+    active_window = prop[0] + (prop[1] << 8) + (prop[2] << 16) + (prop[3] << 24);
+
+    filter_atom = XInternAtom(display, "WM_CLASS", True);
+    status = XGetWindowProperty(display, active_window, filter_atom, 0, 1024,
+                                False, AnyPropertyType, &actual_type,
+                                &actual_format, &nitems, &bytes_after, &prop);
+    check = check_status(status, active_window);
+
     XCloseDisplay(display);
-    return NULL;
-  }
 
-  active_window = prop[0] + (prop[1] << 8) + (prop[2] << 16) + (prop[3] << 24);
+    if (check)
+        return NULL;
 
-  filter_atom = XInternAtom(display, "WM_CLASS", True);
-  status = XGetWindowProperty(display, active_window, filter_atom, 0, 1024,
-                              False, AnyPropertyType, &actual_type,
-                              &actual_format, &nitems, &bytes_after, &prop);
-  check = check_status(status, active_window);
-
-  XCloseDisplay(display);
-
-  if (check)
-    return NULL;
-
-  return (char *)prop;
+    return (char *) prop;
 }
 
 #endif
 
 char *get_active_from_gnome_shell(void) {
-  GError *error = NULL;
-  GDBusConnection *c = NULL;
-  gchar *s = NULL, *parsed = NULL;
+    GError *error = NULL;
+    GDBusConnection *c = NULL;
+    gchar *s = NULL, *parsed = NULL;
 
-  c = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, &error);
+    c = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, &error);
 
-  GVariant *result = NULL;
+    GVariant *result = NULL;
 
-  if (!c) {
-    g_printerr("Error connecting: %s\n", error->message);
-    g_error_free(error);
-    goto out;
-  }
+    if (!c) {
+        g_printerr("Error connecting: %s\n", error->message);
+        g_error_free(error);
+        goto out;
+    }
 
-  result = g_dbus_connection_call_sync(
-      c, "org.gnome.Shell", "/org/gnome/Shell", "org.gnome.Shell", "Eval",
-      g_variant_new("(s)", "global.get_window_actors().map(a=>a.meta_window)."
-                           "find(w=>w.has_focus()).get_wm_class()"),
-      NULL, G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
-  if (!result) {
-    g_printerr("Error: %s\n", error->message);
-    g_error_free(error);
-    goto out;
-  }
+    result = g_dbus_connection_call_sync(
+            c, "org.gnome.Shell", "/org/gnome/Shell", "org.gnome.Shell", "Eval",
+            g_variant_new("(s)", "global.get_window_actors().map(a=>a.meta_window)."
+                                 "find(w=>w.has_focus()).get_wm_class()"),
+            NULL, G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+    if (!result) {
+        g_printerr("Error: %s\n", error->message);
+        g_error_free(error);
+        goto out;
+    }
 
-  s = g_variant_print(result, TRUE);
+    s = g_variant_print(result, TRUE);
 
-  const char *first_occurrence = strchr(s, '"');
-  const char *last_occurrence = strrchr(s, '"');
+    const char *first_occurrence = strchr(s, '"');
+    const char *last_occurrence = strrchr(s, '"');
 
-  if (!first_occurrence || !last_occurrence) {
-    g_printerr("FF-Error: Couldn't parse the application name. Probably new "
-               "application is launching. What we found are: %s\n",
-               s);
-    goto out;
-  }
+    if (!first_occurrence || !last_occurrence) {
+        g_printerr("FF-Error: Couldn't parse the application name. Probably new "
+                   "application is launching. What we found are: %s\n",
+                   s);
+        goto out;
+    }
 
-  int len = last_occurrence - first_occurrence - 1;
-  parsed = malloc(len + 1);
+    int len = last_occurrence - first_occurrence - 1;
+    parsed = malloc(len + 1);
 
-  if (!parsed) {
-    g_printerr("FF-Error: Couldn't parse the application name.\n");
-    goto out;
-  }
+    if (!parsed) {
+        g_printerr("FF-Error: Couldn't parse the application name.\n");
+        goto out;
+    }
 
-  memcpy(parsed, strchr(s, '"') + 1, len);
-  parsed[len] = 0;
+    memcpy(parsed, strchr(s, '"') + 1, len);
+    parsed[len] = 0;
 
 out:
-  if (result)
-    g_variant_unref(result);
-  if (c)
-    g_object_unref(c);
-  if (s)
-    g_free(s);
+    if (result)
+        g_variant_unref(result);
+    if (c)
+        g_object_unref(c);
+    if (s)
+        g_free(s);
 
-  return (char *)parsed;
+    return (char *) parsed;
 }
--- src/home-page.c
+++ src/home-page.c
@@ -1,114 +1,118 @@
 #include "home-page.h"
 
 static struct {
-  GSequence *categories;
-  GSequence *hbox_arr;
+    GSequence *categories;
+    GSequence *hbox_arr;
 } glob_data;
 
 void destroy_cb(gpointer data) { g_free(data); }
 
-void ff_home_page_init(GtkStack *stack) {
-  GtkWidget *temp = gtk_stack_get_child_by_name(stack, "home");
-  if (temp)
-    gtk_container_remove(GTK_CONTAINER(stack), temp);
-  GtkBuilder *home_builder = gtk_builder_new_from_resource(
-      "/org/ccextractor/FastFingers/ui/home-page.ui");
-  GObject *scrolled_window =
-      gtk_builder_get_object(home_builder, "home_scrolled_window");
-  GObject *container = gtk_builder_get_object(home_builder, "home_container");
-
-  cJSON *recent_json = ff_get_application("appdata");
-  cJSON *arr = cJSON_GetObjectItem(recent_json, "recent");
-  if (cJSON_GetArraySize(arr) > 0) {
-    GtkWidget *recent_title = gtk_label_new("Recent");
-    add_style_class(recent_title, "home-header");
-    gtk_box_pack_start(GTK_BOX(container), recent_title, FALSE, FALSE, 0);
-
-    GtkWidget *recent_container = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    gtk_style_context_add_class(gtk_widget_get_style_context(recent_container),
-                                "home-hcont");
-    gtk_widget_set_halign(recent_container, GTK_ALIGN_CENTER);
-    gtk_box_pack_start(GTK_BOX(container), recent_container, FALSE, FALSE, 0);
-    for (int i = 0; i < cJSON_GetArraySize(arr); ++i) {
-      GtkWidget *card = ff_card_new("Firefox", 0.04);
-      gtk_box_pack_start(GTK_BOX(recent_container), card, FALSE, FALSE, 0);
-    }
-  }
-
-  glob_data.categories = g_sequence_new(destroy_cb);
-  glob_data.hbox_arr = g_sequence_new(NULL);
-
-  DIR *d;
-  struct dirent *dir;
-  d = opendir("/usr/share/fastfingers/applications");
-  if (d) {
-    while ((dir = readdir(d))) {
-      if (!g_str_has_suffix(dir->d_name, ".json"))
-        continue;
-      if (!strcmp(dir->d_name, "appdata.json"))
-        continue;
-      char *name = g_strndup(dir->d_name, strlen(dir->d_name) - 5);
-
-      cJSON *app = ff_get_application(name);
-
-      char *category = cJSON_GetObjectItem(app, "category")->valuestring;
-      char *app_title = cJSON_GetObjectItem(app, "title")->valuestring;
-
-      int category_idx = -1;
-      for (int i = 0; i < g_sequence_get_length(glob_data.categories); ++i) {
-        if (!strcmp(category, g_sequence_get(g_sequence_get_iter_at_pos(
-                                  glob_data.categories, i)))) {
-          category_idx = i;
-          break;
-        }
-      }
-
-      if (category_idx == -1) {
-        GtkWidget *category_label = gtk_label_new(category);
-        add_style_class(category_label, "home-header");
-        gtk_box_pack_start(GTK_BOX(container), category_label, FALSE, FALSE, 0);
-
-        GtkWidget *category_container =
-            gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-        gtk_style_context_add_class(
-            gtk_widget_get_style_context(category_container), "home-hcont");
-        gtk_widget_set_halign(category_container, GTK_ALIGN_CENTER);
-        gtk_box_pack_start(GTK_BOX(container), category_container, FALSE, FALSE,
-                           0);
-
-        g_sequence_append(glob_data.categories, category);
-        g_sequence_append(glob_data.hbox_arr, category_container);
-
-        category_idx = g_sequence_get_length(glob_data.categories) - 1;
-      }
-
-      double progress = 0;
-      int total = 0, learned = 0;
-      cJSON *group = cJSON_GetObjectItem(app, "group");
-      for (int i = 0; i < cJSON_GetArraySize(group); ++i) {
+double progress_of_app (cJSON *app){
+    int total = 0, learned = 0;
+    cJSON *group = cJSON_GetObjectItem(app, "group");
+    for (int i = 0; i < cJSON_GetArraySize(group); ++i) {
         cJSON *shortcut_category = cJSON_GetArrayItem(group, i);
         cJSON *shortcuts = cJSON_GetObjectItem(shortcut_category, "shortcuts");
         for (int j = 0; j < cJSON_GetArraySize(shortcuts); ++j) {
-          ++total;
-          if (cJSON_GetObjectItem(cJSON_GetArrayItem(shortcuts, j), "learned")
-                  ->valueint)
-            ++learned;
+            ++total;
+            if (cJSON_GetObjectItem(cJSON_GetArrayItem(shortcuts, j), "learned")
+            ->valueint)
+                ++learned;
         }
-      }
-
-      progress = total ? learned / total : 0;
-
-      GtkWidget *card = ff_card_new(app_title, progress);
-      GtkBox *box = g_sequence_get(
-          g_sequence_get_iter_at_pos(glob_data.hbox_arr, category_idx));
-      gtk_box_pack_start(box, card, FALSE, FALSE, 0);
-
-      g_free(name);
-      cJSON_Delete(app);
     }
-    closedir(d);
-  }
 
-  gtk_stack_add_named(GTK_STACK(stack), GTK_WIDGET(scrolled_window), "home");
-  gtk_widget_show_all(GTK_WIDGET(scrolled_window));
+    return total ? learned / total : 0;
+}
+void ff_home_page_init(GtkStack *stack) {
+    GtkWidget *temp = gtk_stack_get_child_by_name(stack, "home");
+    if (temp)
+        gtk_container_remove(GTK_CONTAINER(stack), temp);
+    GtkBuilder *home_builder = gtk_builder_new_from_resource(
+            "/org/ccextractor/FastFingers/ui/home-page.ui");
+    GObject *scrolled_window =
+            gtk_builder_get_object(home_builder, "home_scrolled_window");
+    GObject *container = gtk_builder_get_object(home_builder, "home_container");
+
+    cJSON *recent_json = ff_get_application("appdata");
+    cJSON *recentApps = cJSON_GetObjectItem(recent_json, "recent");
+    if (cJSON_GetArraySize(recentApps) > 0) {
+        GtkWidget *recent_title = gtk_label_new("Recent");
+        add_style_class(recent_title, "home-header");
+        gtk_box_pack_start(GTK_BOX(container), recent_title, FALSE, FALSE, 0);
+
+        GtkWidget *recent_container = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+        gtk_style_context_add_class(gtk_widget_get_style_context(recent_container),
+                                    "home-hcont");
+        gtk_widget_set_halign(recent_container, GTK_ALIGN_CENTER);
+        gtk_box_pack_start(GTK_BOX(container), recent_container, FALSE, FALSE, 0);
+        for (int i = 0; i < cJSON_GetArraySize(recentApps); ++i) {
+            char *appTitle = cJSON_GetArrayItem(recentApps, i)->valuestring;
+            char *name = ff_simplify_title(appTitle);
+            cJSON *app = ff_get_application(name);
+            double progress = progress_of_app (app);
+            GtkWidget *card = ff_card_new(appTitle, progress);
+            gtk_box_pack_start(GTK_BOX(recent_container), card, FALSE, FALSE, 0);
+        }
+    }
+
+    glob_data.categories = g_sequence_new(destroy_cb);
+    glob_data.hbox_arr = g_sequence_new(NULL);
+
+    DIR *d;
+    struct dirent *dir;
+    d = opendir("/usr/share/fastfingers/applications");
+    if (d) {
+        while ((dir = readdir(d))) {
+            if (!g_str_has_suffix(dir->d_name, ".json"))
+                continue;
+            if (!strcmp(dir->d_name, "appdata.json"))
+                continue;
+            char *name = g_strndup(dir->d_name, strlen(dir->d_name) - 5);
+
+            cJSON *app = ff_get_application(name);
+
+            char *category = cJSON_GetObjectItem(app, "category")->valuestring;
+            char *app_title = cJSON_GetObjectItem(app, "title")->valuestring;
+
+            int category_idx = -1;
+            for (int i = 0; i < g_sequence_get_length(glob_data.categories); ++i) {
+                if (g_str_equal(category, g_sequence_get(g_sequence_get_iter_at_pos(
+                                              glob_data.categories, i)))) {
+                    category_idx = i;
+                    break;
+                }
+            }
+
+            if (category_idx == -1) {
+                GtkWidget *category_label = gtk_label_new(category);
+                add_style_class(category_label, "home-header");
+                gtk_box_pack_start(GTK_BOX(container), category_label, FALSE, FALSE, 0);
+
+                GtkWidget *category_container =
+                        gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+                gtk_style_context_add_class(
+                        gtk_widget_get_style_context(category_container), "home-hcont");
+                gtk_widget_set_halign(category_container, GTK_ALIGN_CENTER);
+                gtk_box_pack_start(GTK_BOX(container), category_container, FALSE, FALSE,
+                                   0);
+
+                g_sequence_append(glob_data.categories, category);
+                g_sequence_append(glob_data.hbox_arr, category_container);
+
+                category_idx = g_sequence_get_length(glob_data.categories) - 1;
+            }
+
+            GtkWidget *card = ff_card_new(app_title, progress_of_app(app));
+            GtkBox *box = g_sequence_get(
+                    g_sequence_get_iter_at_pos(glob_data.hbox_arr, category_idx));
+            gtk_box_pack_start(box, card, FALSE, FALSE, 0);
+
+            g_free(name);
+            cJSON_Delete(app);
+        }
+        closedir(d);
+    }
+
+    gtk_stack_add_named(GTK_STACK(stack), GTK_WIDGET(scrolled_window), "home");
+    gtk_widget_show_all(GTK_WIDGET(scrolled_window));
 }
